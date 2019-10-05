866c5cc95432ed3f4503f15eb5198558d383632f
Kuo-Wei Chung <kuowei.chung@rutgers.edu>
2019-10-05 14:57:57 -0400

    Update Parking Lot.java

--- Parking Lot.java
+++ Parking Lot.java
@@ -1,1 +1,27 @@
+public enum VehicleSize {
+  SMALL, MEDIUM, BIG;
+}
 
+public abstract Vehicle {
+  Slot slot;
+  VehicleSize size;
+  int plateNumber;
+  int carType;
+}
+
+class ParkingLot {
+  public ParkingLot() {
+  }
+  
+  public checkIn(Vehicle vehicle) {
+  }
+  
+  public checkOut(Vehicle vehicle) {
+  }
+}
+
+class Slot {
+  String slotId;
+  Vehicle vehicle;
+  boolean isOccupied;
+}
